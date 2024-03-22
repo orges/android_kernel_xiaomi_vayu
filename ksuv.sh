ce16d7a6ab6f07e96e934326e66bed6e41bce763
orges <orges@orgesified.com>
2024-03-22 10:35:55 +0000

    kernelsu: get version directly from Makefile
     * im tired of playing cat and mouse games
    
    Signed-off-by: orges <orges@orgesified.com>

--- ksuv.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -x
-sed -i "s/ifeq (.*)/ifeq (,,)/g;s/ifndef KSU_EXPECTED_SIZE/KSU_GIT_VERSION ?= 69\nexport KSU_GIT_VERSION\n\$(eval KSU_VERSION=\$(shell expr 10000 + $\(KSU_GIT_VERSION) + 200))\nccflags-y += -DKSU_VERSION=\$(KSU_VERSION)\n\nifndef KSU_EXPECTED_SIZE/g" drivers/kernelsu/Makefile
-export KSU_GIT_VERSION=$(curl -I -k "https://api.github.com/repos/tiann/KernelSU/commits?per_page=1" | sed -n '/^[Ll]ink:/ s/.*"next".*page=\([0-9]*\).*"last".*/\1/p')
